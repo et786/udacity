8f0a8a7cf333d3078fb8fd5deeb21d8657334d70
ahmed-t-syed <Ahmed>
2022-01-29 23:38:25 -0700

    Further building file structure

--- imageProcessingAPI/build/index.js
+++ imageProcessingAPI/build/index.js
@@ -1,1 +1,12 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = __importDefault(require("express"));
+var index_1 = __importDefault(require("./routes/index"));
+var app = (0, express_1.default)();
+var port = 7861;
+app.use('/', index_1.default);
+// Start Express server
+app.listen(port);
---
+++ imageProcessingAPI/src/index.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import routes from './routes/index';
+
+const app = express();
+const port = 7861;
+
+
+
+
+app.use('/', routes);
+// Start Express server
+app.listen(port);
+
+
